8f70dfe0c806a28ffeafe19c713bf8fabcfb6d12
Bhavana-Pisupati <bhavs3012@gmail.com>
2021-12-06 22:48:15 +0530

    Steque using resizable arrays

--- Steque.java
+++ Steque.java
@@ -30,13 +30,15 @@ import java.util.NoSuchElementException;
  *
  */
 public class Steque<Item> implements Iterable<Item> {
-
-
+    private Item[] s;
+    private int N;
+    private int INIT_CAPACITY=10;
     /**
      * constructs a steque object.
      */
     public Steque() {
-
+        N=0;
+        s = (Item[]) new Object[INIT_CAPACITY];
     }
     
     
@@ -45,7 +47,13 @@ public class Steque<Item> implements Iterable<Item> {
      * @param item Item to be inserted.
      */
     public void enqueue(Item item) {
-
+        if (item == null) throw new IllegalArgumentException();
+        if (N == s.length) resize(2*s.length);
+        if( N==0 ) s[0]=item;
+        else{
+            for(int i=s.length-1; i > 0;i--) s[i] = s[i-1];
+        s[0]=item;}
+        N++;
     }
     
     
@@ -54,7 +62,10 @@ public class Steque<Item> implements Iterable<Item> {
      * @param item Item to be inserted.
      */
     public void push(Item item) {
-
+        if (item == null) throw new IllegalArgumentException();
+        if (N == s.length) resize(2*s.length);
+        s[N]=item;
+        N++;
     }
     
     /**
@@ -62,7 +73,12 @@ public class Steque<Item> implements Iterable<Item> {
      * @return Item object from steque.
      */
     public Item pop() {
-
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        Item item=s[N-1];
+        s[N-1]=null;
+        N--;
+        if (N > 0 && N == s.length/4) resize(s.length/2);
+        return item;
     }
     
     /**
@@ -70,7 +86,7 @@ public class Steque<Item> implements Iterable<Item> {
      * @return true if steque is empty, false otherwise.
      */
     public boolean isEmpty() {
-
+        return N==0;
     }
     
     /**
@@ -78,7 +94,15 @@ public class Steque<Item> implements Iterable<Item> {
      * @return size as integer.
      */
     public int size() {
-
+        return N;
+    }
+    // resize the underlying array holding the elements
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
+        }
+        s = copy;
     }
     
     /**
@@ -87,6 +111,28 @@ public class Steque<Item> implements Iterable<Item> {
      * 
      */
     public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
 
+    // an iterator, doesn't implement remove() since it's optional
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i;
+
+        public ReverseArrayIterator() {
+            i = N-1;
+        }
+
+        public boolean hasNext() {
+            return i >= 0;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return s[i--];
+        }
     }
 }
